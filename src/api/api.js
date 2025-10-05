a522963ead2b65ed9f49f95f552d60e62892cf62
Ramkumar <thalar616@gmail.com>
2025-10-06 04:00:32 +0530

    Updated backend API link to Render deployment

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 const BASE_URL = "https://protfolio-backend-691q.onrender.com/api/v1/contact/createContact";
-=======
-const BASE_URL = "https://protfolio-backend-3ndo.onrender.com/api/v1/contact/createContact";
->>>>>>> e4c2a09352e4322219ac57259dc81892a969df1e
 
 export const createContact = async (contactData) => {
   try {
