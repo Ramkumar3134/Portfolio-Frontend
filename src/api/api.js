9d627c8680a9ce2fa1c0fb02d33fe51568829286
Ramkumar <thalar616@gmail.com>
2025-10-06 14:41:20 +0530

    Updated API URL to backend

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const BASE_URL = "https://protfolio-backend-691q.onrender.com/api/v1/contact/cre
 
 export const createContact = async (contactData) => {
   try {
-    const response = await axios.post(`${BASE_URL}/createContact`, contactData);
+    const response = await axios.post(BASE_URL, contactData);
     return response.data;
   } catch (error) {
     console.error("Error sending contact:", error.response?.data || error.message);
